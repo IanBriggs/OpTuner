4c073c30f649ea7b2b32aff2703373198755d479
Ian Briggs <ibriggsutah@gmail.com>
2021-03-29 17:22:29 -0700

    Getting github ci working

--- requirements/build.sh
+++ requirements/build.sh
@@ -1,27 +1,21 @@
 #!/bin/bash
 
+
 set -e
 
-
-SUCCESS=0
-
 SCRIPT_LOCATION="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 LOG="${SCRIPT_LOCATION}/log.txt"
 rm -f "${LOG}"
 
-pushd () {
-    command pushd "$@" > /dev/null
-}
-
-popd () {
-    command popd > /dev/null
-}
-
+SUCCESS=0
 function finish {
     if [ "$SUCCESS" == 0 ]
     then
 	echo "OpTuner requirements failed to build."
 	echo "See ${LOG} for details."
+        echo ""
+        tail "${LOG}"
     else
 	echo "Success"
     fi
@@ -29,55 +23,71 @@ function finish {
 trap finish EXIT
 
 
-# FPTaylor with my_sin
+
+
+# FPTaylor with indicies
 echo "Installing FPTaylor"
-if [ -f "$SCRIPT_LOCATION/FPTaylor/done_fptaylor" ]; then
-    echo "FPTaylor already installed"
+if [ -f "${SCRIPT_LOCATION}/FPTaylor/done_fptaylor" ]; then
+    echo "  FPTaylor already installed"
 else
-    pushd "$SCRIPT_LOCATION"
+    cd "${SCRIPT_LOCATION}"
+
+    echo "  Cleaning build location"
     rm -rf FPTaylor
+
+    echo "  Downloading FPTaylor (indices branch)"
     git clone https://github.com/soarlab/FPTaylor.git &>> "${LOG}"
-    pushd FPTaylor
-    git checkout indices
+    cd "${SCRIPT_LOCATION}/FPTaylor"
+    git checkout indices &>> "${LOG}"
+
+    echo "  Building FPTaylor"
     make &>> "${LOG}"
+
+    echo "  Done"
     touch done_fptaylor
-    popd
-    popd
 fi
+
 
 # gelpia
 echo "Installing Gelpia"
-if [ -f "$SCRIPT_LOCATION/gelpia/done_gelpia" ]; then
-    echo "Gelpia already installed"
+if [ -f "${SCRIPT_LOCATION}/gelpia/done_gelpia" ]; then
+    echo "  Gelpia already installed"
 else
-    pushd "$SCRIPT_LOCATION"
+    cd "${SCRIPT_LOCATION}"
+
+    echo "  Cleaning build location"
     rm -rf gelpia
+
+    echo "  Downloading Gelpia"
     git clone https://github.com/soarlab/gelpia.git &>> "${LOG}"
-    pushd gelpia
+
+    echo "  Building Gelpia requirements"
+    cd "${SCRIPT_LOCATION}/gelpia/requirements"
     SUCCESS=-1
-    pushd requirements
-    sed "s|downloads|versaweb.dl|g" build.sh -i
     ./build.sh | sed "s|^|    |g"
-    popd
     SUCCESS=0
+
+    echo "  Building Gelpia"
+    cd "${SCRIPT_LOCATION}/gelpia"
     make &>> "${LOG}"
+
+    echo "Done"
     touch done_gelpia
-    popd
-    popd
 fi
 
+
 # symbolic link gelpia
-pushd "$SCRIPT_LOCATION"
-pushd FPTaylor
+echo "Connecting Gelpia to FPTaylor"
+cd "${SCRIPT_LOCATION}/FPTaylor"
 ln -s ../gelpia gelpia
-popd
-popd
+
 
 # Debug enviroment source file
-pushd "$SCRIPT_LOCATION"
+cd "${SCRIPT_LOCATION}"
 rm -f debug_enironment.sh
 echo "export PATH=${SCRIPT_LOCATION}/FPTaylor:${SCRIPT_LOCATION}/gelpia/bin:\$PATH" >> debug_enironment.sh
 echo "export PYTHONPATH=${SCRIPT_LOCATION}/gelpia/bin:\$PYTHON_PATH" >> debug_enironment.sh
-popd
 
+
+# Done
 SUCCESS=1
