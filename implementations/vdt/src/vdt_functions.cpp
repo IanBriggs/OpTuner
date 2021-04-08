361f2900e59499a5ef54120dc6af7e7744b292ee
Ian Briggs <ianbriggsutah@gmail.com>
2021-04-08 21:05:34 +0000

    adding cos to vdt

--- implementations/vdt/src/vdt_functions.cpp
+++ implementations/vdt/src/vdt_functions.cpp
@@ -8,6 +8,11 @@
 
 
 
+double cos_vdt(double x) {
+  return vdt::fast_cos(x);
+}
+
+
 double exp_vdt(double x) {
   return vdt::fast_exp(x);
 }
@@ -28,6 +33,11 @@ double tan_vdt(double x) {
 }
 
 
+
+
+double cosf_vdt(double x) {
+  return (double) vdt::fast_cosf((float) x);
+}
 
 
 double expf_vdt(double x) {
