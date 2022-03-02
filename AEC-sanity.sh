c27fef3c5e0f179b239ac5fd3e90ede0e2bc4a33
Ian Briggs <ianbriggsutah@gmail.com>
2022-03-02 16:17:45 -0700

    Eval part 0

--- AEC-sanity.sh
+++ AEC-sanity.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+export SCRIPT_LOCATION=$(readlink -f $(dirname $0))
+cd ${SCRIPT_LOCATION}
+
 set -e
 set -x
 
@@ -7,6 +10,7 @@ set -x
 ./bin/optuner benchmarks/exp1x.fpcore
 cd implementations/timing
 make
+mkdir -p json
 ./bin/time_exp1x > json/time_exp1x.json
 ./scripts/pink_graph.py json/time_exp1x.json
-eog time_exp1x.png
+eog aggregate.png zoomed_aggregate.png
